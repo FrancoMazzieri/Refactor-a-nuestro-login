17d3ef388aed7b9cb22e888bad0c9aac7371ed97
Franco Mazzieri <mazzierifranco@hotmail.com>
2024-06-10 13:26:06 -0300

    1

--- src/config/passport-github.config.js
+++ src/config/passport-github.config.js
@@ -1,16 +1,15 @@
 const passport = require('passport')
 const { Strategy } = require('passport-github2')
 const User = require('../models/user')
-//const hashingUtils = require('../utils/hashing')
 const { clientID, clientSecret, callbackURL } = require('./github.private')
 
 const initializeStrategy = () => {
-
+    
     passport.use('github', new Strategy({
         clientID,
         clientSecret,
         callbackURL
-    }, async(_accessToken,_refreshToken, profile, done)=>{
+    }, async (_accessToken, _refreshToken, profile, done) => {
         try {
             console.log(profile)
 
@@ -34,18 +33,73 @@ const initializeStrategy = () => {
             done(err)
         }
     }))
+    // estrategia para el registro de usuarios
+    passport.use('register', new Strategy({
+        passReqToCallback: true, // habilitar el parámetro "req" en el callback de abajo
+        usernameField: 'email'
+    }, async (req, username, password, done) => {
 
+        const { firstName, lastName, age, email } = req.body
 
-    passport.serializeUser((user, done) => {
-        console.log('serialized!', user)
-        done(null, user._id)
-    })
+        try {
+            const user = await User.findOne({ email: username })
+            if (user) {
+                console.log('User already exists!')
 
-    passport.deserializeUser(async (id, done) => {
-        console.log('deserialized!', id)
-        const user = await User.findById(id)
-        done(null, user)
-    })
+                // null como 1er argumento, ya que no hubo error
+                // false en el 2do argumento, indicando que no se pudo registrar
+                return done(null, false)
+            }
+
+            const newUser = {
+                firstName,
+                lastName,
+                age: +age,
+                email,
+                password: createHash(password)
+            }
+            const result = await User.create(newUser)
+
+            // registro exitoso
+            return done(null, result)
+        }
+        catch (err) {
+            done(err)
+        }
+
+    }))
+    passport.use('login', new Strategy({
+        usernameField: 'email'
+    }, async (username, password, done) => {
+        try {
+            const user = await User.findOne({ email: username })
+            if (!user) {
+                console.log('User not found!')
+                return done(null, false)
+            }
+
+            if (!isValidPassword(user, password)) {
+                return done(null, false)
+            }
+
+            return done(null, user)
+        }
+        catch (err) {
+            done(err)
+        }
+    }))
 }
+
+passport.serializeUser((user, done) => {
+    console.log('serialized!', user)
+    done(null, user._id)
+})
+
+passport.deserializeUser(async (id, done) => {
+    console.log('deserialized!', id)
+    const user = await User.findById(id)
+    done(null, user)
+})
+
 
 module.exports = initializeStrategy
