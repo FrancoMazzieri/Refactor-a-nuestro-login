d72baa1126345f977ae1f5f72302f20351feffc4
Franco Mazzieri <mazzierifranco@hotmail.com>
2024-06-10 15:32:44 -0300

    1

--- src/config/github.private.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    appId: '909878',
-    clientID: 'Iv23ctHvwU4As8Hdf6Uw',
-    clientSecret: '5180bbb7d561badfe3ea4a62db293ee927463b8c',
-    callbackURL: 'http://localhost:8080/api/sessions/githubcallback'
-}
